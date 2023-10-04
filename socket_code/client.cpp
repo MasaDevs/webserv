4ceddd7a474074f5753261fb2ad28d44495669c2
libknt <101494707+key-door@users.noreply.github.com>
2023-10-04 20:02:59 +0900

    add sockrt

---
+++ socket_code/client.cpp
@@ -0,0 +1,91 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define SERVER_ADDR "127.0.0.1"
+#define SERVER_PORT 8080
+#define BUF_SIZE 1024
+
+int transfer(int sock) {
+    char send_buf[BUF_SIZE], recv_buf;
+    int send_size, recv_size;
+
+    while (1) {
+
+        /* サーバーに送る文字列を取得 */
+        printf("Input Message...\n");
+        scanf("%s", send_buf);
+
+        /* 文字列を送信 */
+		// ssize_t send(int sockfd, const void *buf, size_t len, int flags);
+		// return_value 正の数値: 実際に送信されたバイト数。 -1: エラー
+        send_size = send(sock, send_buf, strlen(send_buf) + 1, 0);
+        if (send_size == -1) {
+            printf("send error\n");
+            break;
+        }
+
+        /* サーバーからの応答を受信 */
+        recv_size = recv(sock, &recv_buf, 1, 0);
+        if (recv_size == -1) {
+            printf("recv error\n");
+            break;
+        }
+        if (recv_size == 0) {
+            /* 受信サイズが0の場合は相手が接続閉じていると判断 */
+            printf("connection ended\n");
+            break;
+        }
+
+        /* 応答が0の場合はデータ送信終了 */
+        if (recv_buf == 0) {
+            printf("Finish connection\n");
+            break;
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+	int sock;
+	struct sockaddr_in addr;
+
+    /* ソケットを作成 */
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        printf("socket error\n");
+        return -1;
+    }
+
+    /* 構造体を全て0にセット */
+    memset(&addr, 0, sizeof(struct sockaddr_in));
+
+    /* サーバーのIPアドレスとポートの情報を設定 */
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons((unsigned short)SERVER_PORT);
+    addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+
+    /* サーバーに接続要求送信 */
+    printf("Start connect...\n");
+	// int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+	// 接続を試みるソケットのファイルディスクリプタ
+	// return_value 0 成功　-1 エラー
+    if (connect(sock, (struct sockaddr*)&addr, sizeof(struct sockaddr_in)) == -1) {
+        printf("connect error\n");
+        close(sock);
+        return -1;
+    }
+    printf("Finish connect!\n");
+
+    /* 接続済のソケットでデータのやり取り */
+    transfer(sock);
+
+    /* ソケット通信をクローズ */
+    close(sock);
+
+    return 0;
+}
---
+++ socket_code/server.cpp
@@ -0,0 +1,139 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <unistd.h>
+
+#define SERVER_ADDR "127.0.0.1"
+#define SERVER_PORT 8080
+#define BUF_SIZE 1024
+
+int transfer( int sock ) {
+
+	int recv_size;
+	int send_size;
+
+	char recv_buf[BUF_SIZE];
+	char send_buf;
+
+	while( true ) {
+
+		//　特定のソケットから、データを受信するための関数//TCPで使用される。
+		//  ssize_t recv(int sockfd, void *buf, size_t len, int flags);
+		//  return_value 受信したバイト数
+		//  送信側がコネクションを終了
+		//  エラーが発生した場合、-1 
+		recv_size = recv(sock, recv_buf, BUF_SIZE, 0);
+		if( recv_size == -1 ) {
+			std::cout << "recv error\n";
+			break;
+		}
+
+		if ( recv_size == 0 ) {
+			std::cout << "connection ended\n";
+			break;
+		}
+
+		std::cout << recv_buf << std::endl;
+
+		if ( strcmp(recv_buf, "finish") == 0 ) {
+			send_buf = 0;
+			send_size = send(sock, &send_buf, 1, 0);
+			if ( send_size == -1 ) {
+				std::cout << "send error\n";
+				break;
+			}
+			break;
+		} else {
+			send_buf = 1;
+			send_size = send(sock, &send_buf, 1, 0);
+			if ( send_size == -1 ) {
+				std::cout << "send error\n";
+				break;
+			}
+		}
+	}
+	return 0; 
+}
+
+
+
+int main ( ) {
+	int w_addr, c_sock;
+
+	/**
+	 *
+	 	https://learn.microsoft.com/ja-jp/windows/win32/api/ws2def/ns-ws2def-sockaddr_in
+	 	typedef struct sockaddr_in {
+		#if ...
+  			short          sin_family; // トランスポート アドレスのアドレス ファミリ。常にAF_INETにする。
+		#else
+	  		ADDRESS_FAMILY sin_family; 
+		#endif
+ 		 	USHORT         sin_port; // トランスポート プロトコルのポート番号。
+	  		IN_ADDR        sin_addr; //IPv4 トランスポート アドレスを含む IN_ADDR 構造体。
+  			CHAR           sin_zero[8]; // システムで使用するために予約されています。 WSK アプリケーションでは、この配列の内容を 0 に設定する必要があります。
+		} SOCKADDR_IN, *PSOCKADDR_IN;
+*/
+	struct sockaddr_in a_addr;
+
+	// AF_INET == IPv4
+	// SOCK_STREAM  接続を確立する、接続指向であり、信頼性を担保している。
+	// TCP を使用したプロセス間通信に使用される。他には、HTTP通信、FTP、SSHなど
+	w_addr = socket( AF_INET, SOCK_STREAM, 0);
+	if ( w_addr == -1 ) {
+		std::cout << "sockt error" << std::endl;
+		return -1;
+	}
+
+	memset(&a_addr, 0, sizeof(struct sockaddr_in));
+	
+	a_addr.sin_family = AF_INET;
+	//bigエンディアンに直している host to network short
+	a_addr.sin_port = htons((unsigned short)SERVER_PORT);
+	// inet_addr関数は、ipv4アドレスのドット区切りの文字列表記をバイナリ形式(32ビットの整数値)に変換するために使用される。
+	a_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+
+	// socketfd に　ソケットに割り当てるアドレスやポート番号の情報を設定する。
+	if ( bind( w_addr, (const struct sockaddr *)&a_addr, sizeof(a_addr)) == -1 )  {
+			std::cerr << "bind error\n";
+			close(w_addr);
+			return -1;
+	}
+
+	//指定されたソケットが新しい接続要求を受け入れることを待ち受けるようにする
+	// backlogは待ち行列の最大長を表します。この待ち行列とは、サーバがまだ受け入れていない接続要求の数のことです。
+	if ( listen(w_addr, 3) == -1 ) {
+		std::cout << "listen error\n";
+		return -1;
+	}
+	
+	while ( true ) {
+		std::cout << "Waiting connect ...\n";
+		// 新しいクライアントからの接続要求を受け入れる
+		// socketfd = w_addr: listen関数で待機モードにしたソケットのディスクリプタ
+		// struct sockaddr *addr: 接続要求を行ったクライアントのアドレス情報
+		// socklen_t *addrlen sizeof(addr)
+		// return_value : 新しく作成されたソケットのディスクリプタ: w_addrは引き続きlistenをしていて、新しく通信のためのsocketfdを作成して返す。
+		//  acceptはブロッキング関数(  select とかが関わってくる。（非ブロッキングモード）)
+		c_sock = accept(w_addr, NULL, NULL);
+		if ( c_sock == -1 ) {
+			std::cout << "accept error\n";
+			close(w_addr);
+			return -1;
+		}
+		std::cout << "Connected\n";
+
+		transfer(c_sock);
+
+		close(c_sock);
+
+	}
+
+	close(w_addr);
+
+	return 0;
+}
+
