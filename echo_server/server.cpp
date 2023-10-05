b896c585b5fa2bbba4ab6e61bcf05cddb7dddbb1
libknt <101494707+key-door@users.noreply.github.com>
2023-10-05 18:10:28 +0900

    add

--- echo_server/server.cpp
+++ echo_server/server.cpp
@@ -24,7 +24,7 @@ int main() {
     client_socket[i] =0;
   }
 
-  if ( (master_socket = socket(AF_INET, SOCK_STREAM, 0) == 0) ) {
+  if ( (master_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
     perror("socket failed");
     exit(EXIT_FAILURE);
   }
@@ -33,9 +33,10 @@ int main() {
   address.sin_addr.s_addr = INADDR_ANY;
   address.sin_port = htons(PORT);
 
-  if ( bind(master_socket, (struct  sockaddr *)&address, sizeof(address)) < 0 ) {
-    perror("bind failed");
-    exit(EXIT_FAILURE);
+
+  if (bind(master_socket, (struct sockaddr *)&address, sizeof(address)) < 0) {
+      perror("bind failed");
+      exit(EXIT_FAILURE);
   }
 
   if (listen(master_socket, MAX_CLIENTS) < 0) {
@@ -51,46 +52,46 @@ int main() {
 
     FD_SET(master_socket, &readfds);
     max_sd = master_socket;
-  }
   
-  for ( int i=0; i < MAX_CLIENTS; ++i ) {
-    sd = client_socket[i];
-    if ( sd > 0 ) FD_SET(sd, &readfds);
-    if ( sd > max_sd ) max_sd = sd;
-  }
-
-  activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
-
-  if ((activity < 0 )  && (errno != EINTR)) {
-    std::cerr << "select error" << std::endl;
-  }
-
-  if (FD_ISSET(master_socket, &readfds)) {
-    if ((new_socket = accept(master_socket, (struct sockaddr *)&address, (socklen_t *)&addr_len) < 0)) {
-      std::cerr << "accept" << std::endl;
-      exit(EXIT_FAILURE);
+    for ( int i=0; i < MAX_CLIENTS; ++i ) {
+      sd = client_socket[i];
+      if ( sd > 0 ) FD_SET(sd, &readfds);
+      if ( sd > max_sd ) max_sd = sd;
     }
-  }
 
-  std::cout << "New connection form " << inet_ntoa(address.sin_addr) << " : " << ntohs(address.sin_port) << std::endl;
+    activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
 
-  for (int i=0; i < MAX_CLIENTS; ++i) {
-    if ( client_socket[i] == 0) {
-      client_socket[i] = new_socket;
-      break;
+    if ((activity < 0 )  && (errno != EINTR)) {
+      std::cerr << "select error" << std::endl;
     }
-  }
 
-  for (int i=0; i < MAX_CLIENTS; ++i) {
-    sd = client_socket[i];
-    if(FD_ISSET(sd, &readfds)) {
-      valread = read(sd, buffer, BUFFER_SIZE);
-      if (valread == 0) {
-        close(sd);
-        client_socket[i] =0;
-      } else {
-        buffer[valread] = '\0';
-        send(sd, buffer, strlen(buffer),0);
+    if (FD_ISSET(master_socket, &readfds)) {
+      if ((new_socket = accept(master_socket, (struct sockaddr *)&address, (socklen_t *)&addr_len) < 0)) {
+        std::cerr << "accept" << std::endl;
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    std::cout << "New connection form " << inet_ntoa(address.sin_addr) << " : " << ntohs(address.sin_port) << std::endl;
+
+    for (int i=0; i < MAX_CLIENTS; ++i) {
+      if ( client_socket[i] == 0) {
+        client_socket[i] = new_socket;
+        break;
+      }
+    }
+
+    for (int i=0; i < MAX_CLIENTS; ++i) {
+      sd = client_socket[i];
+      if(FD_ISSET(sd, &readfds)) {
+        valread = read(sd, buffer, BUFFER_SIZE);
+        if (valread == 0) {
+          close(sd);
+          client_socket[i] =0;
+        } else {
+          buffer[valread] = '\0';
+          send(sd, buffer, strlen(buffer),0);
+        }
       }
     }
   }
